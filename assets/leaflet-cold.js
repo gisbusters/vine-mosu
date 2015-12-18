4e67ebddc8dba0271a6fcccee3d07ee73e2e936b
mirceaciu <mirceaciu@gmail.com>
2015-12-18 18:03:51 +0200

    santa marker added

--- assets/leaflet-cold.js
+++ assets/leaflet-cold.js
@@ -2,10 +2,25 @@ var map = L.map('map', {
   center: [44.4166,26.1],
   zoom: 7,
   maxZoom: 9,
-  minZoom: 6
+  minZoom: 7
 });
 
-var londonParisRomeBerlinBucarest = [[48.516667, 20.383333], [44.4166,26.1]];
+var traseuA = [[48.516667, 20.383333], [44.4166,26.1]];
+var traseuB = [[48.516667, 20.383333], [44.4166,26.1]];
+var traseuC = [[48.516667, 20.383333], [44.4166,26.1]];
+var traseuD = [[48.516667, 20.383333], [44.4166,26.1]];
+var traseuE = [[48.516667, 20.383333], [44.4166,26.1]];
+
+var santaIcon = L.icon({
+    iconUrl: 'assets/santaHead.png',
+    shadowUrl: 'assets/santaShadow.png',
+
+    iconSize:     [48, 73], // size of the icon
+    shadowSize:   [48, 68], // size of the shadow
+    iconAnchor:   [25, 82], // point of the icon which will correspond to marker's location
+    shadowAnchor: [13, 77],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
 
 var d = new Date();
 var hour = d.getHours();
@@ -19,9 +34,41 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 }).addTo(map);
 
 
-if (hour == 18 && min < 40 ) {
-  var marker2 = L.Marker.movingMarker(londonParisRomeBerlinBucarest,
-      [6000], {autostart: true, loop: true}).addTo(map);
+var marker2 = L.Marker.movingMarker(traseuA,
+    [3000], {autostart: true, loop: true, icon: santaIcon}).addTo(map);
+
+L.marker([44, 26], {icon: santaIcon}).addTo(map);
+L.marker([46, 25], {icon: santaIcon}).addTo(map);
+
+
+// ora 1
+if (hour == 18) {
+
 } else {
-    alert('mosu nu a plecat')
+}
+
+
+// ora 2
+if (hour == 19) {
+
+} else {
+}
+
+
+// ora 3
+if (hour == 20) {
+
+} else {
+}
+
+// ora 4
+if (hour == 21) {
+
+} else {
+}
+
+// ora 5
+if (hour == 22) {
+
+} else {
 }
