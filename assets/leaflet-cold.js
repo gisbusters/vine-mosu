bc36be328e621764360cad7bd706371278c407ee
Mircea <gisbusters@gmail.com>
2015-12-19 14:44:43 +0200

    add testing for hour

--- assets/leaflet-cold.js
+++ assets/leaflet-cold.js
@@ -60,9 +60,18 @@ var presentIcon = L.icon({
     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
 
+var userHour = prompt("indrodu ora de test, in format 24 ore", 18);
+var userMin = prompt("indrodu minutele de test", 00);
+
+var hour = userHour;
+var min = userMin;
+
+
 var d = new Date();
-var hour = d.getHours();
-var min = d.getMinutes();
+//var hour = d.getHours();
+//var min = d.getMinutes();
+
+
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>, <a href="http://geo-spatial.org/">Geo-spatial</a> and <a href="http://gisbusters.com/">GisBusters</a>, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
